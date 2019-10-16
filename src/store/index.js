81f7d9d5c9dbdf80c1324defd6921562da31608f
Viet Trung <tranviettrung92@gmail.com>
2019-10-16 15:31:54 +0700

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,10 @@ import rootReducer from '../reducers';
 const configureStore = () => {
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLs_EXTENSION__ && window.__REDUX_DEVTOOLs_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  return store;
 };
+
+export default configureStore;
